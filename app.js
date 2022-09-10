56139683a35be7d023d31679a45fe11b43f96c07
Rustam-Asdanov <asdanovrustam@gmail.com>
2022-09-10 15:43:00 +0400

    model is created

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./databases/connection");
+const player_route = require("./routes/player_route");
 const port = 3000;
 
 require("dotenv").config();
 
 app.use(express.json());
+
+app.use("/api/v1/player", player_route);
 
 const start = async () => {
   try {

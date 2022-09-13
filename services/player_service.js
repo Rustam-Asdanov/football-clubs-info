5be2009251a8cc3a44576c1b1cfc386c2e578a4a
Rustam-Asdanov <asdanovrustam@gmail.com>
2022-09-13 16:40:11 +0400

    some updates in player service

--- services/player_service.js
+++ services/player_service.js
@@ -1,12 +1,36 @@
-const { PlayerModel: Player } = require("../models/Player");
+const Team = require("../models/Team");
 
 const getPlayers = async () => {
-  return await Player.find({});
+  let player = new Array();
+
+  await Team.find({}).then((result) => {
+    result.forEach((team) => {
+      player = player.concat(team.players);
+    });
+  });
+
+  console.log(player);
+
+  return player;
 };
 
 const getSomePlayer = async (id) => {
-  return await Player.findById(id);
+  let player = "";
+
+  await Team.find({ "players._id": id }).then((result) => {
+    const team = result[0].name;
+    const players = result[0].players;
+
+    const player = players.filter(
+      (x) => x._id === ObjectID("632076912b3ed5ac112f7ab4")
+    );
+    console.log(player);
+  });
+
+  return player;
 };
+
+// db.teams.find({"players.fullname": "Thomas Muller"}, {"players.$":1})
 
 const createPlayer = async (body) => {
   return await Player.create(body);

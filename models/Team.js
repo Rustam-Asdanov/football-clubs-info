9bee1875830429452bfd6ae749a1af4fa8fdf332
Rustam-Asdanov <asdanovrustam@gmail.com>
2022-09-19 20:05:54 +0400

    now we can add own logo to team and load it on base

--- models/Team.js
+++ models/Team.js
@@ -2,11 +2,31 @@ const mongoose = require("mongoose");
 const { PlayerSchema: playerSchema } = require("./Player");
 
 const teamSchema = new mongoose.Schema({
-  name: String,
-  country: String,
-  league: String,
-  coach: String,
-  balance: String,
+  name: {
+    type: String,
+    default: "My team",
+  },
+  country: {
+    type: String,
+    default: "Mars",
+  },
+  league: {
+    type: String,
+    default: "Other",
+  },
+  coach: {
+    type: String,
+    default: "empty",
+  },
+  balance: {
+    type: String,
+    default: "1000000$",
+  },
+  logo: {
+    type: String,
+    default:
+      "https://img.freepik.com/premium-vector/football-logo-design_680400-15.jpg?w=2000",
+  },
   players: [playerSchema],
 });
 
